e13ee983a30133bcccafcca6017469f766df6b91
Andres <im.andrestorres@gmail.com>
2021-03-11 22:11:09 -0600

    Commited progress

---
+++ src/componentes/Header.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react'
+
+// function Header(props){
+    
+//     console.log(props)
+//     return (
+//         <Fragment>
+//             <h1>{props.titulo}</h1>
+//         </Fragment>
+//     )
+// }
+
+const Header = props =>(
+    <h1>{props.titulo}</h1>
+)
+
+
+export default Header
